1c844b83e93f38957f4e4cfc5bb5608001054f12
Andari757 <matviyskrypka@gmail.com>
2022-05-30 00:35:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { nanoid } from 'nanoid'
 import ContactForm from "components/ContactForm/ContactForm"
 import ContactList from "components/ContactList/ContactList"
 import Filter from "components/Filter/Filter"
 
 export function App() {
-  const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem("contacts")) ? JSON.parse(localStorage.getItem("contacts")) : [])
-  const [filter, setFilter] = useState("")
+  const [contacts, setContacts] = useState([])
+  const [filter, setFilter] = useState("");
+  const one = useRef(true);
 
-  useEffect(() => { localStorage.setItem("contacts", JSON.stringify(contacts)) }, [contacts])
+  useEffect(() => {
+    setContacts(JSON.parse(localStorage.getItem("contacts")) ?
+      JSON.parse(localStorage.getItem("contacts")) :
+      [])
+  }, [])
+
+  useEffect(() => {
+    if (one.current) {
+      one.current = false;
+      return;
+    }
+    localStorage.setItem("contacts", JSON.stringify(contacts))
+  }, [contacts])
+
   const addContact = (data) => {
     if (contacts.find(contact => contact.name === data.name)) {
       alert(`${data.name} already exists`)
@@ -21,29 +35,25 @@ export function App() {
         number: data.number
       }
       return [...prev, contact]
-
     })
   }
-  function getFilteredBooks() {
+
+  function getFilteredContacts() {
     if (!filter) {
       return contacts;
     }
     return contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()))
-
   }
+
   const filterContacts = (e) => {
     setFilter(e.target.value)
   }
-  const remove = (name) => {
-    setContacts((prev) => {
-      return prev.filter(contact => contact.name !== name)
 
-    }
-    )
+  const remove = (name) => {
+    setContacts((prev) => prev.filter(contact => contact.name !== name))
   }
 
-
-  const data = getFilteredBooks()
+  const data = getFilteredContacts()
   return (
     <div className="app">
       <div className="phone-book">
